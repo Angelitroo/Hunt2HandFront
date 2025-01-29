1d4b8e64286f5ddac86f7efd44a2759bf5990117
Angelitro <amedinillaandrada@safareyes.es>
2025-01-29 10:40:15 +0100

    crear producto front hecho

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {MenuInferiorComponent} from "../menu-inferior/menu-inferior.component";
 import {IonicModule} from "@ionic/angular";
+import {addIcons} from "ionicons";
+import {star} from "ionicons/icons";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-chat',
@@ -9,13 +12,38 @@ import {IonicModule} from "@ionic/angular";
   standalone: true,
   imports: [
     MenuInferiorComponent,
-    IonicModule
+    IonicModule,
+    NgClass
   ]
 })
 export class ChatComponent  implements OnInit {
+  estrellaSeleccionada: number = 0;
 
-  constructor() { }
+
+  constructor() {
+    addIcons({ star });
+  }
 
   ngOnInit() {}
+
+  pulsada1() {
+    this.estrellaSeleccionada = 1;
+  }
+
+  pulsada2() {
+    this.estrellaSeleccionada = 2;
+  }
+
+  pulsada3() {
+    this.estrellaSeleccionada = 3;
+  }
+
+  pulsada4() {
+    this.estrellaSeleccionada = 4;
+  }
+
+  pulsada5() {
+    this.estrellaSeleccionada = 5;
+  }
 
 }
