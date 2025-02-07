fbc32898254d5765a809b53d1c6a0025468770a4
Javiermr03 <fmoyarivero@safareyes.es>
2025-02-07 08:34:09 +0100

    Configurado animaciones en perfil, creado boton seguir, y eliminar y modificar productos2

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -41,9 +41,6 @@ export class PerfilComponent implements OnInit {
   perfilId: number | null = null;
 
 
-
-
-
   constructor(
     private perfilesService: PerfilesService,
     private authService: AuthService,
