3b319966b285b8f568a4a22b8cf70336f5d24892
Angelitro <amedinillaandrada@safareyes.es>
2025-02-12 13:38:18 +0100

    Reseña full, mostrar media en perfil

--- src/app/dentro-chat/dentro-chat.component.ts
+++ src/app/dentro-chat/dentro-chat.component.ts
@@ -5,13 +5,15 @@ import { IonicModule } from "@ionic/angular";
 import { DatePipe, NgClass, NgForOf, NgIf } from "@angular/common";
 import { PerfilesService } from "../services/perfiles.service";
 import { addIcons } from "ionicons";
-import { send } from "ionicons/icons";
+import {send, star} from "ionicons/icons";
 import { MensajeService } from "../services/mensaje.service";
 import { Mensaje } from "../modelos/Mensaje";
 import { FormsModule } from "@angular/forms";
 import { AuthService } from "../services/auth.service";
 import { Chat } from "../modelos/Chat";
 import { ChatService } from "../services/chat.service";
+import {ResenaService} from "../services/resena.service";
+import { Resena } from "../modelos/Resena";
 
 @Component({
   selector: 'app-dentro-chat',
@@ -33,7 +35,16 @@ export class DentroChatComponent implements OnInit {
   idEmisor: number = 0;
   idReceptor: number = 0;
 
+
+  estrellaSeleccionada: number = 0;
+  estrellasSeleccionadas: { [key: number]: boolean } = {
+    1: false, 2: false, 3: false, 4: false, 5: false
+  };
+  yaValorado: boolean = false;
+
+
   perfiles: { [key: number]: any } = {};
+
   mensajes: Mensaje[] = [];
   nuevoMensaje: string = '';
   nombreReceptor: string = '';
@@ -45,9 +56,10 @@ export class DentroChatComponent implements OnInit {
     private perfilService: PerfilesService,
     private mensajeService: MensajeService,
     private authService: AuthService,
-    private chatService: ChatService
+    private chatService: ChatService,
+    private resenaService: ResenaService
   ) {
-    addIcons({'send': send });
+    addIcons({ 'star': star, 'send': send });
   }
 
   ngOnInit() {
@@ -58,6 +70,7 @@ export class DentroChatComponent implements OnInit {
       next: (chat: Chat) => {
         this.idReceptor = chat.id_creador === this.idEmisor ? chat.id_receptor : chat.id_creador;
         this.loadPerfilReceptor(this.idReceptor);
+        this.verificarSiYaValoro();
       },
       error: (err) => console.error('Error al obtener el chat:', err)
     });
@@ -81,6 +94,7 @@ export class DentroChatComponent implements OnInit {
         },
       });
     }
+    console.log("perfil emisor: ", idEmisor);
   }
 
   loadPerfilReceptor(perfilId: number) {
@@ -93,6 +107,7 @@ export class DentroChatComponent implements OnInit {
         },
       });
     }
+    console.log("perfil receptor: ", perfilId);
   }
 
   enviarMensaje() {
@@ -119,5 +134,62 @@ export class DentroChatComponent implements OnInit {
       },
       error => console.error('Error enviando mensaje', error)
     );
+  }
+
+  verificarSiYaValoro() {
+    this.resenaService.buscarResena(this.idEmisor, this.idReceptor).subscribe({
+      next: (data) => {
+        console.log('Respuesta de buscarResena:', data);
+        if (data.id_perfilvalorador === this.idEmisor && data.id_perfilvalorado === this.idReceptor) {
+          this.yaValorado = true;
+          this.establecerEstrellasSeleccionadas(data.valoracion);
+        }
+        else {
+          console.log('No ha valorado aún.');
+          this.yaValorado = false;
+          this.estrellaSeleccionada = 0;
+          this.establecerEstrellasSeleccionadas(0);
+        }
+      },
+      error: (err) => {
+        console.error('No existe ninguna reseña con:', 'Emisor:', this.idEmisor, 'Receptor', this.idReceptor, err);
+        this.yaValorado = false;
+        this.estrellaSeleccionada = 0;
+        this.establecerEstrellasSeleccionadas(0);
+      }
+    });
+  }
+
+
+  abrirValoracion(numero: number) {
+    if (this.yaValorado) return;
+
+    const confirmacion = confirm(`¿Valorar con ${numero} estrella${numero > 1 ? 's' : ''}?`);
+    if (confirmacion) {
+      this.estrellaSeleccionada = numero;
+      this.establecerEstrellasSeleccionadas(numero);
+      this.confirmarValoracion();
+      this.yaValorado = true;
+    }
+  }
+
+  establecerEstrellasSeleccionadas(numero: number) {
+    for (let i = 1; i <= 5; i++) {
+      this.estrellasSeleccionadas[i] = i <= numero;
+    }
+  }
+
+  confirmarValoracion() {
+    const nuevaResena = {
+      id: 0,
+      valoracion: this.estrellaSeleccionada,
+      id_perfilvalorado: this.idReceptor,
+      id_perfilvalorador: this.idEmisor
+    };
+    this.resenaService.crearResena(nuevaResena, this.idEmisor, this.idReceptor)
+      .subscribe(() => {
+        console.log(`Valoración confirmada: ${this.estrellaSeleccionada} estrella(s)` );
+        this.yaValorado = true;
+      });
   }
 }
--- src/app/modelos/Resena.ts
+++ src/app/modelos/Resena.ts
@@ -1,4 +1,4 @@
-export interface Reseña {
+export interface Resena {
   id: number;
   valoracion: number;
   id_perfilvalorado?: number;
